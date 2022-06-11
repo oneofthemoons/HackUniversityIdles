aa2d807e6cbae1379e53413597d91647431aa577
Максим Гречаниченко <mun@MacBook-Pro-Maksim.local>
2022-06-11 20:30:30 +0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# HUI - Hack the University Idles
+
+This header-only library will help you solve boring university tasks easier than ever
---
+++ hack_university_idles.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+#include <exception>
+
+namespace idle {
+
+bool IsDigit(char c, size_t base = 10) {
+    static const std::string available =
+        "0123456789"
+        "abcdefghijklmnopqrstuvwxyz";
+    if (base > available.size()) {
+        throw std::runtime_error(__func__": base is more than 36 is unavailable");
+    }
+    bool rv = false;
+    for (size_t i = 0; i < base; ++i) {
+        if (c == available[i]) {
+            rv = true;
+            break;
+        }
+    }
+    return rv;
+}
+
+bool IsInt(const std::string& str, size_t base = 10) {
+    bool rv = !str.empty();
+    size_t i = 0;
+    if (rv) {
+        if (str[i] == '-') {
+            ++i;
+        }
+        if (i == str.size()) {
+            rv = false;
+        }
+        for (; i < str.size(); ++i) {
+            if (!IsDigit(str[i], base)) {
+                rv = false;
+                break;
+            }
+        }
+    }
+    return rv;
+}
+
+bool IsUInt(const std::string& str, size_t base = 10) {
+    bool rv = !str.empty();
+    size_t i = 0;
+    if (rv) {
+        for (; i < str.size(); ++i) {
+            if (!IsDigit(str[i], base)) {
+                rv = false;
+                break;
+            }
+        }
+    }
+    return rv;
+}
+
+}
